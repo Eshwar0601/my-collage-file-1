ecd30a0fd891a0ee7f4deafd20f9e6b590984c12
Eshwar0601 <40708201+Eshwar0601@users.noreply.github.com>
2018-09-12 10:31:37 +0530

    Add files via upload

---
+++ greater.py
@@ -0,0 +1,10 @@
+#find greater number
+x = int(input("value of x :") )
+y = int(input("value of y :") )
+
+
+if (x > y):
+	print("x is the largest number ")
+else:
+	print("y is the greatesrt mnumber")
+
