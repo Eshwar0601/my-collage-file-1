0c5aa2215e84ca03d59801de138efca0f4d8eeaf
Eshwar0601 <40708201+Eshwar0601@users.noreply.github.com>
2018-09-19 08:48:30 +0530

    Update dice.py

--- dice.py
+++ dice.py
@@ -1,14 +1,15 @@
-#dice
+#dice program
 import random
 
-x = int(input(" press 1 to roll the dice   and 0 to quit : "))
-print("Do u wanna print a random number")
-if(x == 1):
-	print(random.randint(1,6))
 
-if(x == 0):
-	print("ok game ends here !!!")
-
+i = 0
+while True:
+    x = int(input("press 1 to roll and 0 to quit"))
+    if(x==1):
+        print(random.randint(1,6))
+    else:
+        print("sorry u lose")
+        exit()
 
 
 
