fcd07b8946a3cf253012145be0d1c3d8e844ecf6
Eshwar0601 <40708201+Eshwar0601@users.noreply.github.com>
2018-10-05 09:27:01 +0530

    Update tic.py

--- tic.py
+++ tic.py
@@ -1,5 +1,3 @@
-import os
-import time
 import random
 
 board = [" "," "," "," "," "," "," "," "," "," "]
@@ -22,20 +20,36 @@ def print_borad():
     print("       |             |       ")
 
 
+# x player 
 
 while True:
     print_borad()
-    choice = int(input("please chose an empty slot"))
+    choice_x = int(input("please chose an empty slot for 'X'"))
+    
+    if(board[choice_x] == ' '):
+        board[choice_x] ='x'
+        print_borad()        
 
-
-    if board[choice] == ' ':
-        board[choice] ='x'
-    else:
-        print("sorry please select an empty slot")
-
-    #chweack for win
-    if(board[1]=='x' and board[2]== 'x' and board[3]=='x'):
+    elif(board[1]=='x' and board[2]== 'x' and board[3]=='x' or board[4]=='x' and board[5]== 'x' and board[6]=='x'     or board[7]=='x' and board[8]== 'x' and board[9]=='x'or board[1]=='x' and board[4]== 'x' and board[7]=='x'or board[2]=='x' and board[5]== 'x' and board[8]=='x'        or board[3]=='x' and board[6]== 'x' and board[9]=='x'    or board[1]=='x' and board[5]== 'x' and board[9]=='x'   or board[3]=='x' and board[5]== 'x' and board[7]=='x'):
         print_borad()
         print("x wins!!!")
         break
+
+    else:
+        print("sorry please select an empty slot for 'X'")
+
+    choice_y = int(input("please chose an empty slot for 'o'"))
+    if(board[choice_y] == ' '):
+        board[choice_y] ='o'
+        print_borad()
+
+    #cheack for o win  
+    elif(board[1]=='o' and board[2]== 'o' and board[3]=='o' or board[4]=='o' and board[5]== 'o' and board[6]=='o'     or board[7]=='o' and board[8]== 'o' and board[9]=='o'or board[1]=='o' and board[4]== 'o' and board[7]=='o'or board[2]=='o' and board[5]== 'o' and board[8]=='o'        or board[3]=='o' and board[6]== 'o' and board[9]=='o'    or board[1]=='o' and board[5]== 'o' and board[9]=='o'   or board[3]=='o' and board[5]== 'o' and board[7]=='o'):
+        print_borad()
+        print("o wins!!!")
+        break
+           
+    else:
+        print("sorry please select an empty slot for 'X'")
+
     
